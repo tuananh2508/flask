3836dedc5ffeab80f4d1435311ae537854a518ea
Macbook <macbook@TuanAnh.lan>
2023-09-06 10:44:25 +0700

    devops: test github action workflow

--- sample.py
+++ sample.py
@@ -14,7 +14,7 @@ def hello_world():
 
 @app.route('/greet_ta')
 def greet_ta():
-    return 'Hello, TA!!'
+    return 'Hello, TA!!!'
 
 if __name__ == '__main__':
     app.run(host=os.getenv('FLASK_HOST', '127.0.0.1'), port=int(os.getenv('FLASK_PORT', 8080)))
